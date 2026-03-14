12925ceff9f9c7a39c4aefa3f99bb921bf507052
ZechsVariety <mortimer@promail.ca>
2026-03-14 16:12:43 -0400

    p2: main menu setup
    gives you all the options
    
    also added exit and invalid input checker

--- StudentManager.java
+++ StudentManager.java
@@ -1,8 +1,60 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentManager
 {
-    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n~~~ WELCOME TO THE STUDENT MANAGER! ~~~\n");
+
+        boolean running = true;
+        while(running)
+        {
+            System.out.println("~ Main Menu ~"
+            + "\n1. Add a new student"
+            + "\n2. Delete a student"
+            + "\n3. Search for a student"
+            + "\n4. Display students and teachers"
+            + "\n5. Exit");
+
+            String input = scanner.nextLine();
+
+            switch(input)
+            {
+                //add new student
+                case "1":
+
+                break;
+
+                case "2":
+                    
+                break;
+
+                case "3":
+                    
+                break;
+
+                case "4":
+                    
+                break;
+
+                //exit
+                case "5":
+                    System.out.println("Goodbye.");
+                    running = false;
+                break;
+
+                //invalid input
+                default:
+                    System.out.println("ERROR: INVALID INPUT\n");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
 }
 
 abstract class Person
